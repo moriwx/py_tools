2f4cf0f60335fb07003a82eb3b4ab464badf919a
moriwx <64824605+moriwx@users.noreply.github.com>
2021-04-24 18:07:15 +0800

    Update pdf_MCM.py
    add OCR

--- pdf_MCM.py
+++ pdf_MCM.py
@@ -1,14 +1,10 @@
 import os
-from urllib.request import urlretrieve
-
 root = r'E:\\MCM21\\'
 mcm = 'https://www.comap-math.com/mcm/2021Certs/21'
-for i in range(1, 10000) :
-    urlretrieve(mcm + str(i).rjust(5,'0') + '.pdf', root + str(i).rjust(5,'0') + '.pdf')
-    
+
+# Merge PDFs
 from PyPDF2 import PdfFileMerger
 
-target_path = '/Users/kwsy/Desktop'
 pdf_lst = [f for f in os.listdir(root) if f.endswith('.pdf')]
 pdf_lst = [os.path.join(root, filename) for filename in pdf_lst]
 file_merger = PdfFileMerger()
@@ -16,25 +12,28 @@ for pdf in pdf_lst:
     file_merger.append(pdf)
 file_merger.write(root + "merge.pdf")
 
+# Transfer JPGs to PDF
 import img2pdf
 
-p = []
-for i in range(1, 130) :
-	p.append('E:\\a\\0 (' + str(i) + ').jpg')
+p = ['1.jpg', '2.jpg']
 a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
 layout_fun = img2pdf.get_layout_fun(a4inpt)
-with open("E:/b/name.pdf", "wb") as f:
-	f.write(img2pdf.convert(p, layout_fun=layout_fun))
+with open("new.pdf", "wb") as f:
+    f.write(img2pdf.convert(p, layout_fun=layout_fun))
 
+# Transfer PDF to JPG, then recognize the text
 from pdf2image import convert_from_path
-pages = convert_from_path(r'E:\MCM21\00007.pdf', 100)
-for page in pages:
-    page.save(r'E:\out.jpg', 'JPEG')
-    
 from PIL import Image
-import os
-import pytesseract 
+import re
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'D:\ProgramFiles\Tesseract-OCR\tesseract.exe'
 
-img_path = r'E:\out.jpg'
-text=pytesseract.image_to_string(Image.open(img_path),lang='eng')
-print(text)
+for i in range(5, 28000) :
+    fno = str(i).rjust(5,'0')
+    print(i, end='\r', flush=True)
+    try : 
+        page = convert_from_path(mcm + fno + '.pdf', 90)
+        if re.search("Mathematical", pytesseract.image_to_string(page[0], lang='eng')) :
+            page[0].save(root + fno + '.jpg', 'JPEG')
+    except :
+        continue
